f6a04c532968f6d4385027acbe9548bce6ebe09d
IvanMuzik <ivanmuz86@gmail.com>
2024-08-13 14:16:42 +0300

    add labels names in VC2

--- habits/VC2.swift
+++ habits/VC2.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class VC2: UIViewController {
 
+    let nameHabitLabel = UILabel()
+    let timeLapsLabel = UILabel()
+    let characterSexLabel = UILabel()
+    let timerLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+        nameHabitLabel.text = "Название привычки"
+        
+
+
     }
     
 
