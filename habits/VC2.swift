8c6a7e88db904b0bd2bfc940d05f0216ab52dc76
IvanMuzik <ivanmuz86@gmail.com>
2024-08-13 13:59:56 +0300

    created screens and view controllers

---
+++ habits/VC2.swift
@@ -0,0 +1,21 @@
+//
+//  VC2.swift
+//  habits
+//
+//  Created by Ivan Muzalevski on 13.08.24.
+//
+
+import UIKit
+
+class VC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
--- habits/ViewController.swift
+++ habits/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var oneTV: UITextView!
+    @IBOutlet weak var twoTV: UITextView!
+    @IBOutlet weak var threeTV: UITextView!
+    @IBOutlet weak var fourTV: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func nextButtonAction(_ sender: UIButton) {
+        
+    }
+    
 }
 
